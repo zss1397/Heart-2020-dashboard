af60f6ba0ade738ccec9d77b0f409a5191123552
zss1397 <zss17@mail.aub.edu>
2025-06-22 14:50:18 +0300

    Update app (4).py

--- app (4).py
+++ app (4).py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import os
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Heart Disease Dashboard", layout="wide")
 
